9ba79bad9e7fbefc0c95740b992999172be0f235
BuildTools <jromphf@library.rochester.edu>
2018-05-25 15:11:17 -0400

    added some loaders

--- src/index.js
+++ src/index.js
@@ -3,45 +3,32 @@ import * as _THREE from 'three';
 
 // Addons
 import loadAddOns from './utils';
-
-// examples
-import drawCube from '../examples/Cube';
-import drawCubeWithLight from '../examples/CubeWithLight';
-import {cubeOnPlane, addLights, changeLightPositions} from '../examples/CubeWithManyLights';
-
+import { createCubes, rotateCubes, addCubesToScene } from './CubeStuff';
 const THREE = loadAddOns(_THREE);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.shadowMap.enabled = true;
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.z = 30;
+const scene = new THREE.Scene();
 
-let camera, controls, lights, cube, plane;
-let scene = new THREE.Scene();
+let cubes = createCubes(100, 0.25);
+addCubesToScene(cubes, scene);
+
 /*
 * All of our example functions will take scene, camera, mesh, geometry, and material params
 * and modify them.
 * We'll then call them in the init function.
 */
 
-function initCamera() {
-  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
-	camera.position.z = 1;
-  controls = new THREE.OrbitControls(camera, renderer.domElement);
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	document.body.appendChild( renderer.domElement );
-}
-
 function init() {
-  [cube, plane] = cubeOnPlane(scene);
-  lights = addLights(scene, 5);
-  initCamera();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 }
 
 function animate() {
   requestAnimationFrame(animate);
-  changeLightPositions(lights);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.05;
-	renderer.render( scene, camera );
+  rotateCubes(cubes);
+  renderer.render(scene, camera);
 }
 
 init();
