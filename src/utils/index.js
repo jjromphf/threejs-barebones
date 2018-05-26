d741d8fbec929ee14ce293e0785a9074e4df2965
BuildTools <jromphf@library.rochester.edu>
2018-05-26 09:12:55 -0400

    added obj and mtl loaders to utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,10 @@
 import loadOrbitControls from './OrbitControls';
+import loadOBJLoader from './loaders/OBJLoader';
+import loadMTLLoader from './loaders/MTLLoader';
 
 export default function loadAddOns(_three) {
   loadOrbitControls(_three);
+  loadOBJLoader(_three);
+  loadMTLLoader(_three);
   return _three;
 }
